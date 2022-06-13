acc2d289a969a0c7f08933b99f59e79aab878640
unknown <jewelgreeho@gmail.com>
2022-06-13 13:06:16 +0600

    modify to app file

--- app.js
+++ app.js
@@ -69,7 +69,9 @@ app.get("/rockets/:rocket_id", async (req, res, next) => {
     }
 });
 
-
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
 
 app.listen( process.env.PORT || 3000, () => console.log("🚀 on port 3000"));
 
